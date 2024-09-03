c1be47f4ec4cf08012a3a04aea273364608bd4b5
Dalia Manfrin <dalia.manfrin@gmail.com>
2024-09-03 22:01:48 +0200

    edits to include reading config in code
    typo fixed

--- rw.py
+++ rw.py
@@ -2,16 +2,23 @@ import random
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import configparser
 
-num_nodes = 20  # M
-initial_particles = 2
+# Load configuration using configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Assign parameters from the config file
+num_nodes = config.getint('Simulation', 'num_nodes')
+initial_particles = config.getint('Simulation', 'initial_particles')
+n_max = config.getint('Simulation', 'n_max')
+num_time_steps = config.getint('Simulation', 'num_time_steps')
+
+# Initialize the network
 network = [initial_particles] * num_nodes
 
 N = num_nodes * initial_particles
 M = num_nodes
-
-n_max = 20
-num_time_steps = 1000
 
 G = nx.cycle_graph(num_nodes)
 pos = nx.circular_layout(G)
@@ -19,29 +26,26 @@ pos = nx.circular_layout(G)
 particle_counts = []
 
 for t in range(num_time_steps):
-
     new_network = network.copy()
 
     for i in range(num_nodes):
-
-
         if network[i] > 0:
-
             direction = random.randint(0, 1)  # move left or right (1 right, 0 left)
 
             if direction == 1:
-                neighbor = (i + 1) % num_nodes  
+                neighbor = (i + 1) % num_nodes
             else:
-                neighbor = (i - 1) % num_nodes  
+                neighbor = (i - 1) % num_nodes
 
             if network[neighbor] < n_max:
                 new_network[i] -= 1
                 new_network[neighbor] += 1
-            
+
     network = new_network
     if t % 10 == 0:  # append particle counts every 10 steps
         particle_counts.append(network.copy())
-        
+
+# Flatten and visualize the data
 flattened_data = [item for sublist in particle_counts for item in sublist]
 data = np.array(flattened_data)
 
